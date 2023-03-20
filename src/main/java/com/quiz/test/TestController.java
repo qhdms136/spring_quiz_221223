90566632ecffaec8f9044aad4b93d9f848f86878
DESKTOP-DGKU219\User <qhdms136@naver.com>
2023-03-20 21:48:47 +0900

    20230320수업

--- src/main/java/com/quiz/test/TestController.java
+++ src/main/java/com/quiz/test/TestController.java
@@ -1,12 +1,9 @@
 package com.quiz.test;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @Controller
 public class TestController {
 
